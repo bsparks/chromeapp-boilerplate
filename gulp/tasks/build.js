7ea2a2d690a37b48a39b481e3964b3a40e71f67a
Ben Sparks <ben.sparks@pearson.com>
2016-07-20 13:52:49 -0700

    get the basics of angular material working

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -7,8 +7,15 @@ gulp.task('build:bundle', function() {
     return jspm.bundleSFX('src/main', 'build/js/main.js');
 });
 
-gulp.task('build:copy', function() {
-    return gulp.src('chrome/**/*')
+gulp.task('build:copy', ['build:copy:chrome', 'build:copy:assets']);
+
+gulp.task('build:copy:assets', function() {
+    return gulp.src(['assets/**/*'], {base: '.'})
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:copy:chrome', function() {
+    return gulp.src(['chrome/**/*'])
         .pipe(gulp.dest('build/'));
 });
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
-class Foo {
-    constructor() {
-        this.bar = [1, 2, 3];
-    }
+import 'angular-material/angular-material.css!';
+import 'assets/app.css!';
 
-    test() {
-        console.debug(...this.bar);
-    }
-}
+import angular from 'angular';
+import 'angular-material';
 
-let foo = new Foo();
-foo.test();
+angular
+    .element(document)
+    .ready(function () {
+        let appName = 'boilerplate-app';
+        let body = document.getElementsByTagName('body')[0];
+        let app = angular
+            .module(appName, ['ngMaterial']);
+
+        app.config(function ($mdIconProvider) {
+            $mdIconProvider
+                .defaultIconSet('assets/mdi.svg')
+        });
+
+        app.run($rootScope => {
+            let appWin = chrome.app.window.current();
+            $rootScope.closeApp = function() { appWin.close(); };
+            $rootScope.minimizeApp = function() { appWin.minimize(); };
+            $rootScope.maximizeApp = function() { appWin.maximize(); };
+        });
+
+        angular.bootstrap(body, [app.name], { strictDi: false });
+    });
